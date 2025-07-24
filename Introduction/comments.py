268916b55977f6b9fa4e1190c353ecc060dd6fe9
Dheeraj Sonkar <dheerajsonkarmy@gmail.com>
2025-07-24 06:40:16 +0530

    Add initial Python scripts for comments, sample output, and variable demonstration

---
+++ Introduction/comments.py
@@ -0,0 +1,6 @@
+# this is single line comments
+# this is another single line comments
+'''here is a multi-line comment
+this is the second line of the multi-line comment
+this is the third line of the multi-line comment'''
+print("hello brother")
---
+++ Introduction/variable.py
@@ -0,0 +1,5 @@
+name = "Dheeraj"
+age= 22
+height = 5.5
+print("Name:", name, ",Age:", age, ",Height:", height)
+print(name, age, height)
