749e7cb70da35628ecf916b27efe758deb5a9913
Dheeraj Sonkar <dheerajsonkarmy@gmail.com>
2025-08-09 10:41:14 +0530

    Update index.py

--- index.py
+++ index.py
@@ -1,1 +1,8 @@
-print("hello")
+name = "dheeraj sonkar"
+age = "22"
+print("my name is ", name, "and my age is", age)
+print(f"my name is {name} and my age is {age}")
+name = input("what is your name : ")
+age = input("what is your age : ")
+print(f"my name is {name}")
+print(f"my age is {age}")
